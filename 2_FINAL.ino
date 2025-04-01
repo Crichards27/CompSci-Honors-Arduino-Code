f42b888f7bc06ea4ef18993fd884eacc98b9b046
Crichards27 <crichards27@cbastudents.org>
2025-04-01 09:19:46 -0400

    Update 2_FINAL.ino
    fixed buton not being detected

--- 2_FINAL.ino
+++ 2_FINAL.ino
@@ -1,9 +1,10 @@
-const int red1 = 2, yellow1 = 3, green1 = 4;
-const int red2 = 5, yellow2 = 6, green2 = 7;
-const int pedestrianButton = 8, pedestrianLight = 9;
+const int red1 = 3, yellow1 = 4, green1 = 5;    //North-South
+const int red2 = 6, yellow2 = 7, green2 = 8;    //East-West
+const int pedestrianButton = 2, pedestrianLight = 9;
 const int buzzer = 10; 
 
-bool pedestrianRequest = false; 
+volatile bool pedestrianRequest = false;
+volatile unsigned long lastInterruptTime = 0;
 
 void setup() {
   pinMode(red1, OUTPUT);
@@ -14,13 +15,14 @@ void setup() {
   pinMode(green2, OUTPUT);
   pinMode(pedestrianLight, OUTPUT);
   pinMode(buzzer, OUTPUT);  
-  
   pinMode(pedestrianButton, INPUT_PULLUP); 
+  Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(pedestrianButton), requestPedestrianCrossing, FALLING);
 }
 
 void loop() {
   if (pedestrianRequest) {
+    Serial.println("Pedestrian button pressed");
     activatePedestrianCrossing();
     pedestrianRequest = false;
   } else {
@@ -28,37 +30,30 @@ void loop() {
   }
 }
 
-// Normal traffic light cycle
 void normalTrafficCycle() {
-  // Signal 1: Red, Signal 2: Green (5s)
   digitalWrite(red1, HIGH);
   digitalWrite(green2, HIGH);
   delay(5000);
   
-  // Signal 2: Yellow (3s)
   digitalWrite(green2, LOW);
   digitalWrite(yellow2, HIGH);
   delay(3000);
   
-  // Signal 2: Red, Signal 1: Green (5s)
   digitalWrite(yellow2, LOW);
   digitalWrite(red2, HIGH);
   digitalWrite(red1, LOW);
   digitalWrite(green1, HIGH);
   delay(5000);
   
-  // Signal 1: Yellow (3s)
   digitalWrite(green1, LOW);
   digitalWrite(yellow1, HIGH);
   delay(3000);
   
-  // Reset for next cycle
   digitalWrite(yellow1, LOW);
   digitalWrite(red1, HIGH);
   digitalWrite(red2, LOW);
 }
 
-// Pedestrian crossing function
 void activatePedestrianCrossing() {
   for (int i = 0; i < 3; i++) {
     digitalWrite(red1, HIGH);
@@ -78,7 +73,6 @@ void activatePedestrianCrossing() {
     delay(500);
   }
 
-  
   digitalWrite(pedestrianLight, HIGH);
   for (int i = 0; i < 10; i++) {  
     tone(buzzer, 1000); 
@@ -89,7 +83,10 @@ void activatePedestrianCrossing() {
   digitalWrite(pedestrianLight, LOW);
 }
 
-
 void requestPedestrianCrossing() {
-  pedestrianRequest = true;
+  unsigned long interruptTime = millis();
+  if (interruptTime - lastInterruptTime > 200) {  
+    pedestrianRequest = true;
+    lastInterruptTime = interruptTime;
+  }
 }
